b0dba6e1c8672989158f3b3111ce3a339b78519d
Karen Garcia <gar240730@spengergasse.at>
2025-05-09 20:38:41 +0200

    Delete 2025-03__Server_json directory

--- 2025-03__Server_json/fetch_student_v1.js
+++
@@ -1,7 +0,0 @@
-const p = fetch("http://localhost:8000/student");
-// p ist ein promise
-p.then((response) => response.json())
-    // response.json() gibt ein promise zurück
-    .then((student) => {
-        console.log(student);
-    });
--- 2025-03__Server_json/fetch_student_v2.js
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:8000/student")
-    .then((response) => response.json())
-    .then((student) => {
-        console.log(student);
-    });
